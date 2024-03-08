384d7d58aeee4272b07e7f11729f12fad7d56867
colba5776 <cbayne@oakland.edu>
2024-03-08 17:57:53 -0500

    b

---
+++ usedCars.js
@@ -0,0 +1,200 @@
+const usedCars = [
+  {
+    year: 2018,
+    make: "Toyota",
+    model: "Camry",
+    mileage: 30000,
+    price: 18000,
+    color: "Silver",
+    gasMileage: "25 mpg city, 35 mpg highway",
+    image: "./assets/Camry,toyota.webp"
+  },
+  {
+    year: 2016,
+    make: "Honda",
+    model: "Civic",
+    mileage: 45000,
+    price: 14000,
+    color: "White",
+    gasMileage: "30 mpg city, 40 mpg highway",
+    image: "./assets/Honda Civic.jpg",
+  },
+  {
+    year: 2017,
+    make: "Ford",
+    model: "Fusion",
+    mileage: 35000,
+    price: 16000,
+    color: "Black",
+    gasMileage: "28 mpg city, 38 mpg highway",
+    image: "./assets/Ford Fusion.jpg",
+  },
+  {
+    year: 2019,
+    make: "Nissan",
+    model: "Altima",
+    mileage: 25000,
+    price: 17000,
+    color: "Blue",
+    gasMileage: "27 mpg city, 36 mpg highway",
+    image: "./assets/Nissan Altima.jpg",
+  },
+  {
+    year: 2015,
+    make: "Chevrolet",
+    model: "Malibu",
+    mileage: 50000,
+    price: 12000,
+    color: "Red",
+    gasMileage: "25 mpg city, 37 mpg highway",
+    image: "./assets/Chevrolet Malibu.jpg",
+  },
+  {
+    year: 2016,
+    make: "Volkswagen",
+    model: "Passat",
+    mileage: 40000,
+    price: 15000,
+    color: "Gray",
+    gasMileage: "29 mpg city, 40 mpg highway",
+    image: "./assets/Volkswagen Passat.jpg",
+  },
+  {
+    year: 2020,
+    make: "Hyundai",
+    model: "Elantra",
+    mileage: 22000,
+    price: 16000,
+    color: "Silver",
+    gasMileage: "30 mpg city, 41 mpg highway",
+    image: "./assets/elantra, hyundai.webp",
+  },
+  {
+    year: 2014,
+    make: "Subaru",
+    model: "Outback",
+    mileage: 60000,
+    price: 14000,
+    color: "Green",
+    gasMileage: "22 mpg city, 30 mpg highway",
+    image: "./assets/Subaru Outback.jpg",
+  },
+  {
+    year: 2017,
+    make: "Mazda",
+    model: "CX-5",
+    mileage: 32000,
+    price: 19000,
+    color: "Blue",
+    gasMileage: "24 mpg city, 31 mpg highway",
+    image: "./assets/cx5, mazda.webp",
+  },
+  {
+    year: 2018,
+    make: "Kia",
+    model: "Sorento",
+    mileage: 28000,
+    price: 17000,
+    color: "White",
+    gasMileage: "22 mpg city, 29 mpg highway",
+    image: "./assets/Kia Sorento.jpg",
+  },
+  {
+    year: 2015,
+    make: "Dodge",
+    model: "Challenger",
+    mileage: 30000,
+    price: 24000,
+    color: "Black",
+    gasMileage: "19 mpg city, 30 mpg highway",
+    image: "./assets/Dodge Challenger.jpg",
+  },
+  {
+    year: 2017,
+    make: "Cadillac",
+    model: "XT5",
+    mileage: 28000,
+    price: 32000,
+    color: "Red",
+    gasMileage: "19 mpg city, 27 mpg highway",
+    image: "./assets/XT5, cadillac.jpg",
+  },
+  {
+    year: 2018,
+    make: "Jaguar",
+    model: "F-PACE",
+    mileage: 26000,
+    price: 38000,
+    color: "Blue",
+    gasMileage: "18 mpg city, 23 mpg highway",
+    image: "./assets/F-pace,jaguar.webp",
+  },
+  {
+    year: 2019,
+    make: "Tesla",
+    model: "Model S",
+    mileage: 18000,
+    price: 55000,
+    color: "Black",
+    gasMileage: "Electric (370 miles per charge)",
+    image: "./assets/Tesla Model S.jpg",
+  },
+  {
+    year: 2020,
+    make: "Porsche",
+    model: "Cayenne",
+    mileage: 22000,
+    price: 68000,
+    color: "White",
+    gasMileage: "20 mpg city, 26 mpg highway",
+    image: "./assets/cayenne,porsche.webp",
+  },
+  {
+    year: 2017,
+    make: "Lexus",
+    model: "ES",
+    mileage: 29000,
+    price: 26000,
+    color: "White",
+    gasMileage: "21 mpg city, 30 mpg highway",
+    image: "./assets/Lexus ES.jpg",
+  },
+  {
+    year: 2016,
+    make: "BMW",
+    model: "5 Series",
+    mileage: 32000,
+    price: 27000,
+    color: "Black",
+    gasMileage: "23 mpg city, 34 mpg highway",
+    image: "./assets/5,BMW.webp",
+  },
+];
+
+console.log(usedCars);
+
+function createCarCards() {
+  const carContainer = document.getElementById("scroll_block");
+
+  usedCars.forEach((car) => {
+    car.price = car.price.toLocaleString();
+    car.mileage = car.mileage.toLocaleString();
+    const card = document.createElement("div");
+    card.className = "car-card";
+    card.innerHTML = `
+    <img src="${car.image}" alt="${car.make} ${car.model} Image">
+    <hr>
+          <h2>${car.make} ${car.model}</h2>
+         
+          <p>Price: $${car.price}</p>
+    <hr>
+     <p>Year: ${car.year}</p>
+    <p> Color: ${car.color} </p>
+    <p> Mileage: ${car.mileage} </p>
+    <p> Miles to the gallon: ${car.gasMileage} </p>
+    <button type="button" class="btn btn-outline-primary">Extra Details</button>
+      `;
+    carContainer.appendChild(card);
+  });
+}
+createCarCards();
